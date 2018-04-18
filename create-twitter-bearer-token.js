5700165ee4a093698c2beb8ca732ef017db2c7e2
lsweatman <lukesweatman@gmail.com>
2018-04-17 21:16:53 -0700

    Twitter and OracleDB support

---
+++ create-twitter-bearer-token.js
@@ -0,0 +1,18 @@
+var request = require('request');
+require('dotenv').config();
+console.log(process.env.CONSUMER_KEY);
+var consumer_key = process.env.CONSUMER_KEY;
+var consumer_secret = process.env.CONSUMER_SECRET;
+var encode_secret = new Buffer(consumer_key + ':' + consumer_secret).toString('base64');
+
+var options = {
+    url: 'https://api.twitter.com/oauth2/token',
+    headers: {
+        'Authorization': 'Basic ' + encode_secret,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
+    body: 'grant_type=client_credentials'
+};
+
+request.post(options, function(error, response, body) {
+    console.log(body); // <<<< This is your BEARER TOKEN !!!
+});
--- index.js
+++ index.js
@@ -1,11 +1,55 @@
+var request = require("request");
 var oracledb = require('oracledb');
-//var Twitter = require('twitter');
-var https = require('https');
+require('dotenv').config();
 
-/*var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  bearer_token: process.env.TWITTER_BEARER_TOKEN
-});*/
+var twitter_api = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+var bearer_token = process.env.BEARER_TOKEN; // the token you got in the last step
+var lastTweetID;
 
-oracle
+var options = {
+    method: 'GET',
+    url: twitter_api,
+    qs: {
+        "screen_name": "realDonaldTrump",
+        "exclude_replies": true,
+        "count": 1
+    },
+    json: true,
+    headers: {
+        "Authorization": "Bearer " + bearer_token
+    }
+};
+
+oracledb.getConnection(
+    {
+        user: process.env.DB_CONNECT_USER,
+        password: process.env.DB_CONNECT_PASSWORD,
+        connectString: process.env.DB_CONNECT_STRING
+    },
+    function(err, connection) {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+      console.log('Connection was successful!');
+  
+      connection.close(
+        function(err) {
+          if (err) {
+            console.error(err.message);
+            return;
+          }
+        });
+  });
+
+setInterval(sendRequest, 60000);
+
+function sendRequest() {
+    request(options, function(error, response, body) {
+        console.dir(body[0].text);
+        if(body[0].id != lastTweetID){
+            lastTweetID = body[0].id;
+            // TODO: insert things into the database
+        }
+    });
+}
